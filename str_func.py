96624ee4928b3081e6cdd68b84ff8bad31106682
Sergey <kapetanvodichka@gmail.com>
2023-06-07 01:56:31 +0500

    new feature

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def up_letters(message):
     Принимает сообщение и меняет все буквы в верхний регистр222
     """
     return message.upper()
+
+def up_first_letter(message):
+    """
+    Принимает сообщение и меняет первую букву каждого слова
+    """
+    return message.upper()
